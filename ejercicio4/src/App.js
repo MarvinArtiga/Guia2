1b540c11f6532835efeb784744c13a85174bd5cf
Marvin Artiga AG230133 <marvin.artigagonzalez@gmail.com>
2024-02-01 09:57:32 -0600

    agregar los ultimos cambios de ejercicio4

--- ejercicio4/src/App.js
+++ ejercicio4/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+"use client";
+import { useState } from "react";
+import styles from "./App.css";
+export default function Home() {
+const [numero, setNumero] = useState(1);
+const [limite, setLimite] = useState(10);
+const [resultado, setResultado] = useState([]);
+const generarTabla = () => {
+const nuevaTabla = [];
+for (let i = 1; i <= limite; i++) {
+nuevaTabla.push(`${numero} x ${i} = ${numero * i}`);
 }
-
-export default App;
+setResultado(nuevaTabla);
+};
+return (
+<main className={styles.main}>
+<div>
+<h2 className={styles.title2}>Tabla de Multiplicar</h2>
+<label className={styles.text}>
+Ingrese un número:
+<input className={styles.input}
+type="number"
+value={numero}
+onChange={(e) => setNumero(parseInt(e.target.value))}
+/>
+</label>
+<br />
+<label className={styles.text}>
+Límite de números a mostrar:
+<input className={styles.input}
+type="number"
+value={limite}
+onChange={(e) => setLimite(parseInt(e.target.value))}
+/>
+</label>
+<br />
+<button className={styles.button} onClick={generarTabla}>Generar
+Tabla</button>
+<hr />
+<h3>Resultado</h3>
+<ul className={styles.ul}>
+{resultado.map((item, index) => (
+<li className={styles.li} key={index}>{item}</li>
+))}
+</ul>
+</div>
+</main>
+);
+}
