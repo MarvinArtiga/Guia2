5722e96eb0dbbe8d32f4a3456ab49da63f38f0de
Marvin Artiga AG230133 <marvin.artigagonzalez@gmail.com>
2024-02-01 21:38:14 -0600

    Terminar el proyecto

--- ejemplo2/src/App.js
+++ ejemplo2/src/App.js
@@ -1,6 +1,8 @@
 import styles from "./App.css";
 const Equipos = ({ equipos }) => {
 return (
+
+    
 <div className={styles.container__list}>
 <h2 className={styles.title}>Equipos de Fútbol</h2>
 {equipos.map((equipo) => (
@@ -11,7 +13,8 @@ return (
 <li className={styles.container__list} key={jugador.id}>
 <strong>{jugador.nombre}</strong>
 <p>Altura: {jugador.Altura}m <br></br> Peso:
-{jugador.Peso}Kg</p>
+{jugador.Peso}Kg</p> <br></br> 
+ <img src="{data.imagen}"></img>
 </li>
 ))}
 </ul>
@@ -26,7 +29,8 @@ const equiposData = [
 {"id": 1,
 "nombre": "Real Madrid",
 "plantilla":[
-{"id": 1,"nombre":"Eden Hazard","Altura":"1.75","Peso":"74Kg"},
+{"id": 1,"nombre":"Eden Hazard","Altura":"1.75","Peso":"74Kg","imagen":"./img/eden.jpg"},
+
 {"id": 2,"nombre":"Gonzalo García","Altura":"1.82","Peso":"74Kg"},
 {"id": 3,"nombre":"Karim Benzema","Altura":"1.85","Peso":"81Kg"}
 ]},
@@ -36,8 +40,19 @@ const equiposData = [
 {"id": 1,"nombre":"Marc-André ter Stegen","Altura":"1.75","Peso":"74Kg"},
 {"id": 2,"nombre":"Iñigo Martinez","Altura":"1.82","Peso":"74Kg"},
 {"id": 3,"nombre":"Gavi","Altura":"1.85","Peso":"81Kg"}
-]}
+]},
 // ... agregar otros equipos
+
+{"id": 3,
+"nombre": "Al-Nassr",
+"plantilla":[
+{"id": 1,"nombre":"Cristiano Ronaldo ","Altura":"1.95","Peso":"75Kg"},
+{"id": 2,"nombre":"Marvin Artiga","Altura":"1.82","Peso":"65Kg"},
+{"id": 3,"nombre":"Emily Artiga","Altura":"1.55","Peso":"25Kg"}
+]}
+
+
+
 ];
 return (
 <main className={styles.main}>
